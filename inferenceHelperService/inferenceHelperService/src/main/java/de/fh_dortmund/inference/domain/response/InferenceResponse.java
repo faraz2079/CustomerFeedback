c1c0e44754d15331e7a973e154347f33e0e9de84
BhanuPDas <bhanu.das001@stud.fh-dortmund.de>
2025-01-24 13:06:28 +0100

    Updating script for local training

--- inferenceHelperService/inferenceHelperService/src/main/java/de/fh_dortmund/inference/domain/response/InferenceResponse.java
+++ inferenceHelperService/inferenceHelperService/src/main/java/de/fh_dortmund/inference/domain/response/InferenceResponse.java
@@ -1,5 +1,7 @@
 package de.fh_dortmund.inference.domain.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Builder;
 import lombok.Data;
 
@@ -7,12 +9,19 @@ import lombok.Data;
 @Builder
 public class InferenceResponse {
 
+	@JsonProperty("sentiment")
 	private String sentiment;
+	@JsonProperty("feedback_score")
 	private float feedbackScore;
+	@JsonProperty("accuracy")
 	private float accuracy;
+	@JsonProperty("cpu_utilization")
 	private float cpuUtilization;
+	@JsonProperty("ram_usage")
 	private float ramUsage;
+	@JsonProperty("cpu_cycles")
 	private int cpuCycle;
+	@JsonProperty("inference_time")
 	private float inferenceTime;
 
 }
