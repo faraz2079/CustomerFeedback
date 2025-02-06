85bbb22b5e8a5b2b69012019885ae77e013404d9
BhanuPDas <bhanu.das001@stud.fh-dortmund.de>
2025-02-06 15:09:23 +0100

    Updating script for local training

--- k6_load_testing/requests.js
+++ k6_load_testing/requests.js
@@ -11,11 +11,11 @@ export const options = {
     scenarios: {
         bulk_requests: {
             executor: 'constant-arrival-rate',
-            rate: 100, // requests per second
+            rate: 150, // requests per second
             timeUnit: '1s',
-            duration: '8m',
-            preAllocatedVUs: 100,
-            maxVUs: 120,
+            duration: '10m',
+            preAllocatedVUs: 150,
+            maxVUs: 300,
         },
     },
     thresholds: {
